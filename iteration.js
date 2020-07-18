4d66ac269d32720f668a15287f3e33d3eba6ad72
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-07-17 18:58:32 -0600

    Chaining array methods

--- iteration.js
+++ iteration.js
@@ -90,41 +90,103 @@
 
 // console.log(users)
 
-const users = [
-    {name: 'Samir', age: 27},
-    {name: 'Angela', age: 33},
-    {name: 'Beatrice', age: 42},
-    {name: 'Shaniqua', age: 30},
-    {name: 'Marvin', age: 23},
-    {name: 'Sean', age: 47}
-  ];
+// const users = [
+//     {name: 'Samir', age: 27},
+//     {name: 'Angela', age: 33},
+//     {name: 'Beatrice', age: 42},
+//     {name: 'Shaniqua', age: 30},
+//     {name: 'Marvin', age: 23},
+//     {name: 'Sean', age: 47}
+//   ];
 
-const userNames = users
-    .filter(user => user.age >= 30)
-    .map(user => user.name);
+// const userNames = users
+//     .filter(user => user.age >= 30)
+//     .map(user => user.name);
 
-console.log(userNames)
+// console.log(userNames)
 
 
-const todos = [
+// const todos = [
+//     {
+//         todo: 'Buy apples',
+//         done: false
+//     },
+//     {
+//         todo: 'Wash car',
+//         done: true
+//     },
+//     {
+//         todo: 'Write web app',
+//         done: false
+//     },
+//     {
+//         todo: 'Read MDN page on JavaScript arrays',
+//         done: true
+//     },
+//     {
+//         todo: 'Call mom',
+//         done: false
+//     }
+// ];
+
+
+// const products = [
+//     { name: 'hard drive', price: 59.99 },
+//     { name: 'lighbulbs', price: 2.59 },
+//     { name: 'paper towels', price: 6.99 },
+//     { name: 'flatscreen monitor', price: 159.99 },
+//     { name: 'cable ties', price: 19.99 },
+//     { name: 'ballpoint pens', price: 4.49 }
+//   ];
+  
+
+
+// const total = products
+//     .filter(product => product.price > 10)
+//     .reduce((sum, product) => {
+//         sum += product.price;
+//         return sum;
+//     }, 0).toFixed(2);
+
+// console.log(total);
+
+const purchaseItems = [
     {
-        todo: 'Buy apples',
-        done: false
+        name: 'apples',
+        dept: 'groceries',
+        price: 2.49
     },
     {
-        todo: 'Wash car',
-        done: true
+        name: 'bread',
+        dept: 'groceries',
+        price: 2.99
     },
     {
-        todo: 'Write web app',
-        done: false
+        name: 'batteries',
+        dept: 'electronics',
+        price: 5.80
     },
     {
-        todo: 'Read MDN page on JavaScript arrays',
-        done: true
+        name: 'eggs',
+        dept: 'groceries',
+        price: 3.99
     },
     {
-        todo: 'Call mom',
-        done: false
+        name: 't-shirts',
+        dept: 'apparel',
+        price: 9.99
     }
 ];
+let groceryTotal;
+
+// groceryTotal should be: 9.47
+// Write your code below
+
+groceryTotal = purchaseItems
+  .filter(item => item.dept === 'groceries')
+  .reduce((total, item) => {
+    total += item.price;
+    return total;
+  });
+
+console.log(groceryTotal);
